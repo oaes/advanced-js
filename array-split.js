7a0148b17a2f266b211bdb5973878ef2790ad94e
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-03-04 22:49:49 +0600

    array slice,splice and join is done

---
+++ array-split.js
@@ -0,0 +1,16 @@
+//slice
+
+number = [1,2,3,4,5,6];
+const cut = number.slice(0,4);
+console.log(cut)
+
+//splice
+
+id = [22,33,44,55,77,99,33,]
+const removed = id.splice(1,3);
+console.log(removed)
+
+//join
+
+const together = id.join(" ");
+console.log(together)
---
+++ closure.js
@@ -0,0 +1,21 @@
+function watch(){
+    let count = 0;
+    return function(){
+        count++;
+        return count;
+    }
+}
+
+const clock1 = watch();
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+
+const clock2 = watch();
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
