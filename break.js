a381071cf93410e989dba583445ec39034d6b76f
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-03-04 23:49:55 +0600

    done

--- break.js
+++ break.js
@@ -1,7 +1,14 @@
-const num = [1,2,3,4,5,7,8]
+const num = [1,2,-3,4,-5,7,8]
 for(let i=0; i<num.length; i++){
     if(num[i]>7){
         break;
+    }
+    console.log(num[i]);
+}
+//continue
+for(let i=0; i<num.length; i++){
+    if(num[i]>0){
+        continue;
     }
     console.log(num[i]);
 }
